7983ba6e2ca13e59a7eb2015ead28cf313d20255
AditShh28 <ak2057338@gmail.com>
2025-11-28 15:43:07 +0530

    fix verify page revalidate error for vercel

--- src/app/(auth)/verify/page.js
+++ src/app/(auth)/verify/page.js
@@ -1,10 +1,5 @@
 "use client";
 
-export const dynamic = "force-dynamic";
-export const fetchCache = "force-no-store";
-export const revalidate = 0;
-export const runtime = "nodejs";
-
 
 import { useEffect } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
